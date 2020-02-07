3a672c27832c82f80e3b79c5c8c46d934da8c42d
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-02-07 15:39:36 -0600

    Lab 2 CS407

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitText(View view) {
+        EditText userInput = (EditText) findViewById(R.id.usernameInput);
+        String username = userInput.getText().toString();
+        // Toast.makeText(MainActivity.this, username, Toast.LENGTH_LONG).show();
+        goToActivity2(username);
+    }
+
+    public void goToActivity2(String s) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("username", s);
+        startActivity(intent);
     }
 }
